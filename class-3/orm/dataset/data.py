f8587a126950df219bbe5037bd37aaebc2bb336b
Juan Pablo Aguirre <juanpablomartinez590@hotmail.com>
2023-03-15 02:22:15 +0100

    main example

---
+++ class-3/orm/dataset/data.py
@@ -0,0 +1,60 @@
+coins = [
+    {
+        "id": 1,
+        "name": "bitcoin",
+        "symbol": "BTC",
+        "status": "active",
+        "category": "coin",
+        "tags": ["mineable", "pow", "sha-256", "store-of-value", "state-channels", "coinbase-ventures-portfolio",
+                 "three-arrows-capital-portfolio", "polychain-capital-portfolio"],
+        "website": "https://bitcoin.org/",
+        "twitter": None,
+        "chat": None,
+        "reddit": "https://reddit.com/r/bitcoin",
+        "source_code": "https://github.com/bitcoin/",
+        "date_launched": None
+    },
+    {
+        "id": 2,
+        "name": "litecoin",
+        "symbol": "LTC",
+        "status": "active",
+        "category": "coin",
+        "tags": ["mineable", "pow", "scrypt", "medium-of-exchange", "binance-chain"],
+        "website": "https://litecoin.org/",
+        "twitter": "https://twitter.com/LitecoinProject",
+        "chat": "https://telegram.me/litecoin",
+        "reddit": "https://reddit.com/r/litecoin",
+        "source_code": "https://github.com/litecoin-project/litecoin",
+        "date_launched": None
+    }
+]
+
+historical_records = [
+    {
+        "date": "2013-07-09",
+        "coin_id": 1,
+        "rank": 1,
+        "market_cap": 873841504.319,
+        "price": 76.694,
+        "high": 78.300,
+        "low": 72.519,
+        "close": 76.694,
+        "percent_change_1h": 0.0222,
+        "percent_change_24h": 0.133,
+        "percent_change_7d": -14.887
+    },
+    {
+        "date": "2013-08-26",
+        "coin_id": 1,
+        "rank": 1,
+        "market_cap": 1393841929.717,
+        "price": 120.0555,
+        "high": 122.754,
+        "low": 119.930,
+        "close": 120.0559,
+        "percent_change_1h": -0.547,
+        "percent_change_24h": -1.867,
+        "percent_change_7d": 0.829
+    }
+]
--- class-3/orm/main.py
+++ class-3/orm/main.py
@@ -1,23 +1,64 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Library, Base
+from sqlalchemy.sql import func
+
+from models import Base, Coin, Historical
+from dataset.data import coins, historical_records
 from database import CONFIG
 
-engine = create_engine("mysql+mysqlconnector://%s:%s@localhost:3306/%s" %
-                       (CONFIG['DB_USER'], CONFIG['DB_PASSWORD'], CONFIG['DB_NAME']))
+engine = create_engine(
+    "mysql+mysqlconnector://%s:%s@localhost:3306/%s" %
+    (CONFIG['DB_USER'], CONFIG['DB_PASSWORD'], CONFIG['DB_NAME'])
+)
+
 connection = engine.connect()
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-l1 = Library(name="universitée lyon 2")
+# insert coins
+for coin in coins:
+    c = Coin(id=coin["id"], name=coin["name"], symbol=coin["symbol"], status=coin["status"], category=coin["category"], tags=coin["tags"])
+    c_instance = session.query(Coin).filter_by(id=coin["id"]).one_or_none()
 
-# user = User(name='John Doe', email='john.doe@example.com')
-# session.add(user)
-# session.commit()
+    if not c_instance:
+        session.add(c)
+        session.commit()
 
-# query all users
-# users = session.query(User).all()
-# for user in users:
-#     print(user.name, user.email)
+# insert historical
+for hr in historical_records:
+    h = Historical(
+        date=hr["date"], coin_id=hr["coin_id"], rank=hr["rank"], market_cap=hr["market_cap"], price=hr["price"],
+        high=hr["high"], low=hr["low"], close=hr["close"], percent_change_1h=hr["percent_change_1h"],
+        percent_change_24h=hr["percent_change_24h"], percent_change_7d=hr["percent_change_7d"])
+
+    h_instance = session.query(Historical).filter_by(date=hr["date"], coin_id=hr["coin_id"]).one_or_none()
+
+    if not h_instance:
+        session.add(h)
+        session.commit()
+
+# get all coins
+coins = session.query(Coin).all()
+for coin in coins:
+    print(f"--> {coin.name} {coin.__dict__}")
+
+# count coins
+print(f"--> total number of coins {session.query(Coin).count()}")
+
+# market cap average for bitcoin
+bitcoin = session.query(Coin).filter_by(symbol="BTC").one_or_none()
+bitcoin_historical = session.query(Historical).filter_by(coin_id=bitcoin.id)
+
+sum = 0
+for bh in bitcoin_historical:
+    sum += bh.market_cap
+avg = sum / bitcoin_historical.count()
+print(f"average --> {avg}")
+
+# market cap average for bitcoin using mysql avg aggregation
+avg = session.query().with_entities(func.avg(Historical.market_cap).label('market_cap_avg')).filter_by(coin_id=bitcoin.id)
+print(f"generate avg sql -->")
+print(avg)
+print(f"query result --> {avg.all()}")
