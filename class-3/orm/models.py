a085f64ca4a108462416dcd3b9178c6fcc36965a
Juan Pablo Aguirre <juanpablomartinez590@hotmail.com>
2023-03-15 02:28:16 +0100

    models

--- class-3/orm/models.py
+++ class-3/orm/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, DateTime, Date
+from sqlalchemy.dialects.mysql import FLOAT as MY_SQL_FLOAT, JSON
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -11,18 +11,30 @@ class Coin(Base):
     __tablename__ = "coin"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(length=50), nullable=False)
-    symbol = Column(String(length=10), nullable=False)
+    name = Column(String(length=100), nullable=False)
+    symbol = Column(String(length=20), nullable=False)
     status = Column(String(length=50), nullable=False)
-    type = Column(String(length=50), nullable=False)
+    category = Column(String(length=50), nullable=True)
+    tags = Column(JSON, nullable=True)
+    website = Column(String(length=256), nullable=True)
+    twitter = Column(String(length=256), nullable=True)
+    chat = Column(String(length=256), nullable=True)
+    reddit = Column(String(length=256), nullable=True)
+    source_code = Column(String(length=256), nullable=True)
+    date_launched = Column(DateTime, nullable=True)
 
 
 class Historical(Base):
     __tablename__ = "historical"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    library_id = Column(Integer, ForeignKey("library.id"))
-    name = Column(String(length=100), nullable=False)
-    type = Column(String(length=50), nullable=False)
-    subject = Column(String(length=100), nullable=False)
-    publication_year = Column(Integer, nullable=False)
+    date = Column(Date, primary_key=True)
+    coin_id = Column(Integer, ForeignKey("coin.id"), primary_key=True)
+    rank = Column(Integer, nullable=False)
+    market_cap = Column(MY_SQL_FLOAT(unsigned=True), nullable=True)
+    price = Column(Float, nullable=False)
+    high = Column(Float, nullable=True)
+    low = Column(Float, nullable=True)
+    close = Column(Float, nullable=True)
+    percent_change_1h = Column(Float, nullable=True)
+    percent_change_24h = Column(Float, nullable=True)
+    percent_change_7d = Column(Float, nullable=True)
